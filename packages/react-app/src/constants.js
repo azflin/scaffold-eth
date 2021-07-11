4e5a438d3438b35a405a8420d6ee677e994cbb59
Allen Lin <allenlin1992@hotmail.com>
2021-07-11 15:05:22 -0400

    edit etherscan id

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -2,7 +2,7 @@
 export const INFURA_ID = "6e758ef5d39a4fdeba50de7d10d08448";
 
 //ToDo: MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "H64KN1CW3CIZ9RE7Q77QGCFMWD71Y67CKI";
 
 // EXTERNAL CONTRACTS vvvv, add the addresses and abi's here.
 export const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
